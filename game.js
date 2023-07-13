e68278b08e4550d271c77c06477f41d2a8f271a1
Felipe Alves <felipealves3580@gmail.com>
2023-07-13 15:28:58 -0300

    Add js

---
+++ game.js
@@ -0,0 +1,31 @@
+function getComputerChoice(){
+    let randomNum = Math.floor(Math.random() * 3 + 1);
+    if(randomNum == 1){
+        return "rock";
+    } else if (randomNum == 2){
+        return "paper";
+    } else return "scissors";
+}
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+
+    if(playerSelection == "rock" && computerSelection == "paper")
+        return "You Lose! Paper beats Rock"
+    else if(playerSelection == "rock" && computerSelection == "scissors"){
+        return "You Win! Rock beats Scissors";
+    } else if(playerSelection == "paper" && computerSelection == "rock"){
+        return "You Win! Paper beats Rock";
+    } else if(playerSelection == "paper" && computerSelection == "scissors"){
+        return "You Lose! Scissors Beats Paper";
+    }  
+    else if(playerSelection == "scissors" && computerSelection == "rock") {
+        return "You Lose! Rock beats Scissors"
+    }  else if (playerSelection == "scissors" && computerSelection == "paper"){
+        return "You Win! Scissors beats Paper";
+    } else return "Draw!"
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
